23f11eaa4096f405353e621f19982003c0f275ab
bordedelcielo <christopher.thrutchley@gmail.com>
2022-08-02 00:55:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.listen(port, () => {
+    console.log(`Express application started at port: ${3000}`)
+})
+
+app.use((req, res, next) => {
+    console.log(`Request type: ${req.method} made at route ${req.originalUrl} at time: ${new Date()}`)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.get('/pineapple', (req, res) => {
+    res.send('best fruit ever')
+})
+
+app.get('/wick', (req, res) => {
+    res.send('great last name')
+})
